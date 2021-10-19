a2e98f4a4854443a74973911e765d2d469cb9b15
kmgliya <kuramaevagaliya28@gmail.com>
2021-10-19 21:40:22 +0600

    add last exam

---
+++ src/components/postModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    id {{id}}
+  </div>
+  <div>
+    body {{body}}
+  </div>
+
+  <button v-on:click="closePostModal">Закрыть</button>
+</template>
+
+<script>
+  import {defineComponent} from 'vue';
+
+  export default defineComponent({
+    name: 'PostModal',
+    props: {
+      id: Number,
+      body: String
+    },
+    methods: {
+      closePostModal() {
+        this.$emit("closePostModal", false)
+      }
+    }
+  })
+</script>
---
+++ src/pages/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post">
+    <div v-for="(post,ind) in posts" :key="ind">
+      <Post :post="post"/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import {defineComponent} from 'vue';
+  import axios from 'axios'
+  import Post from "../components/Post";
+
+  export default defineComponent({
+    name: 'PostList',
+    components: {
+      Post
+    },
+    data() {
+      return {
+        posts: []
+      }
+    },
+
+    mounted() {
+      axios('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+          this.posts = response.data
+        })
+        .catch(error => console.log(error))
+    }
+  })
+</script>
